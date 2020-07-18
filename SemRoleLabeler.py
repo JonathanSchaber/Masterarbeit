e3f4deced1fdb5e6351c77d2cc66b7c488579a61
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-07-18 11:02:19 +0200

    parser-function working, probably not very stable...

--- SemRoleLabeler.py
+++ SemRoleLabeler.py
@@ -12,8 +12,19 @@ from liir.dame.core.representation.Predicate import Predicate
 import parzu_class as parzu
 
 
+def create_ParZu_parser():
+    """Create an ParZu-parser object
+    Args:
+        None
+    Returns:
+        ParZu-parser object
+    """
+    options = parzu.process_arguments()
+    ParZu = parzu.Parser(options)
+    return ParZu
 
-def parse_sentence(parser: parzu_class.Parser, text: str):
+
+def parse_sentence(parser, text):
     """parse sentence and return list of tuples with token and POS-tags
     Args:
         param1: parser Object
@@ -21,21 +32,24 @@ def parse_sentence(parser: parzu_class.Parser, text: str):
     Returns:
         list of tuples of strings
     """
-    
+    tagged_sent = parser.tag([text])[0]
+    splitted_sents = tagged_sent.split("\n")
+    tagged_tuple_list = [(token.split("\t")[0], token.split("\t")[1]) for token in splitted_sents]
+    return tagged_tuple_list
 
 
-def create_dsrl_repr(text: list):
+def create_dsrl_repr(sentences):
     """Wraps the POS-labelled tokens into a DSRL class
     Args:
-        param1: list of 2-tuples of strings [ (<token>, <POS>), ... ]
+        param1: list of lists of 2-tuples of strings [ (<token>, <POS>), ... ]
     Returns:
         DSRL class Text()
     """
     dsrl_text = Text()
 
-    sentence = Sentence([Word(tuple[0]) if tuple[1] != "VVFIN" else Predicate(Word(tuple[0])) for tuple in text])
-
-    dsrl_text.append(sentence)
+    for sentence in sentences:
+        dsrl_sentence = Sentence([Word(tuple[0]) if tuple[1] != "VVFIN" else Predicate(Word(tuple[0])) for tuple in sentence])
+        dsrl_text.append(dsrl_sentence)
     
     for sentence in dsrl_text:
         for predicate in sentence.get_predicates():
@@ -45,7 +59,12 @@ def create_dsrl_repr(text: list):
 
 
 def main(text):
-    print(create_dsrl_repr(text))
+    ParZu_parser = create_ParZu_parser()
+    parsed_text = parse_sentence(ParZu_parser, text)
+    dsrl_text = create_dsrl_repr([parsed_text])
+    import pdb; pdb.set_trace()
+    
 
 if __name__ == "__main__":
-    main([("Der", "ART"), ("Hund", "NN"), ("jagt", "VVFIN"), ("die", "ART"), ("Katze", "ART"), (".", "$.")])
+    main("Gestern sah ich einen Vogel über die Dächer fliegen.")
+    
