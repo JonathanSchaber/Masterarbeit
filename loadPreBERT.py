936c8871fd3ab48661757e7defd390db21f8bbed
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-07-22 17:57:37 +0200

    added function for initializing model and tokenizer

--- loadPreBERT.py
+++ loadPreBERT.py
@@ -1,8 +1,20 @@
 from transformers import BertTokenizer, BertForMaskedLM
 
-tokenizer = BertTokenizer.from_pretrained(path_to_model)
 
-model = BertForMaskedLM.from_pretrained(path_to_model)
+def create_model_and_tokenizer():
+    """Create respective classes and return them
+    Args:
+        param1: str
+    Returns:
+        transformer classes 
+    """
+    tokenizer = BertTokenizer.from_pretrained(path_to_model)
+    model = BertForMaskedLM.from_pretrained(path_to_model)
+
+    return tokenizer, model
+
+    
+
 
 def prepare_sentences(sentences, max_len):
     """Tokenize, pad, add special tokens
@@ -12,8 +24,10 @@ def prepare_sentences(sentences, max_len):
     Returns:
         list of torch.Tensor
     """
-    torch_sentences
+    torch_sentences = []
     for sentence in sentences:
         torch_sentences.append(tokenizer.encode(sentence, add_special_tokens = True, max_length = max_len, pad_to_max_length = True, return_tensors = 'pt')
+
+    return torch_sentences
 
 
