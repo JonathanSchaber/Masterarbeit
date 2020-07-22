68a45ab1a4690bf5c48f7b3d086d5f78e68ddbd5
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-07-22 17:44:30 +0200

    loading data function started

--- loadPreBERT.py
+++ loadPreBERT.py
@@ -4,3 +4,16 @@ tokenizer = BertTokenizer.from_pretrained(path_to_model)
 
 model = BertForMaskedLM.from_pretrained(path_to_model)
 
+def prepare_sentences(sentences, max_len):
+    """Tokenize, pad, add special tokens
+    Args:
+        param1: list of strs
+        param2: int
+    Returns:
+        list of torch.Tensor
+    """
+    torch_sentences
+    for sentence in sentences:
+        torch_sentences.append(tokenizer.encode(sentence, add_special_tokens = True, max_length = max_len, pad_to_max_length = True, return_tensors = 'pt')
+
+
