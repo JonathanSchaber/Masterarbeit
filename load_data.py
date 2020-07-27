d94ed1073887b955f98f7df3cde257d61b5f276c
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-07-27 15:18:20 +0200

    added function to predict SRLs of XNLI

--- load_data.py
+++ load_data.py
@@ -1,5 +1,7 @@
 import csv
 
+from SemRoleLabeler import *
+
 def load_XNLI(path):
     """loads the data from XNLI data set
     Args:
@@ -20,4 +22,25 @@ def load_XNLI(path):
                 counter += 1
 
     return xnli_data, y_mapping
+
+
+def SRL_XNLI(xnli_data, dsrl, parser):
+    """predict semantic roles of xnli data and return new object
+    Args:
+        param1: list of tuples of strs
+        param2: DSRL object
+        param*: ParZu object
+    Returns:
+        list of tuples of strs
+    """
+    srl_xnli = []
+    num_examples = len(xnli_data)
+    for i, example in enumerate(xnli_data):
+        if i % 100 == 0:
+            print("processed the {}th example out of {}...".format(i, num_examples))
+        label, sentence1, sentence2 = example
+        srl_xnli.append((label, sentence1, predict_semRoles(dsrl, process_text(parser, sentence1)), sentence2, predict_semRoles(dsrl, processed(parser, sentence2))))
+
+    return srl_xnli
+
 
