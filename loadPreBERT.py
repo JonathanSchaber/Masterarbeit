297d853859ee71664641e3e9e1c05e9778c3b894
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-07-28 10:48:53 +0200

    started training function

--- loadPreBERT.py
+++ loadPreBERT.py
@@ -1,6 +1,10 @@
 import torch
 
 from torch import nn
+from torch.utils.data import (
+        TensorDataset, random_split
+        DataLoader, RandomSampler, SequentialSampler
+        )
 from transformers import (
         BertTokenizer,
         BertModel,
@@ -18,6 +22,20 @@ class BertBinaryClassifier(nn.Module):
         _, pooled_output = self.bert(tokens)
         linear_output = self.linear(dropout_output)
         proba = self.sigmoid(linear_output)
+        return proba
+
+
+class BertEntailmentClassifier(nn.Module):
+    def __init__(self, path, out_classes=3, dropout=0.1):
+        super(BertEntailmentClassifier, self).__init__()
+        self.bert = BertModel.from_pretrained(path)
+        self.linear = nn.Linear(768, out_classes)
+        self.softmax = nn.LogSoftmax()
+    
+    def forward(self, tokens):
+        _, pooled_output = self.bert(tokens)
+        linear_output = self.linear(dropout_output)
+        proba = self.softmax(linear_output)
         return proba
 
 
@@ -56,6 +74,32 @@ def combine_srl_embs_bert_embs():
     """
     comb_tensor = torch.cat((bert_tensor, srl_tensor), 0)
     pass
+
+
+def fine_tune_BERT(model, data, labels, batch_size=32):
+    """define fine-tuning procedure, write results to file.
+    Args:
+        param1: nn.Model (BERT-model)
+        param2: torch.tensor
+        param3: torch.tensor
+        param4: int
+    Returns:
+        None
+    """
+    dataset = TensorDataset(data, labels)
+    train_size = int(0.9 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    train_dataloader = DataLoader(
+            train_dataset,
+            sampler = RandomSampler(train_dataset),
+            batch_size = batch_size
+        ) 
+    test_dataloader = DataLoader(
+            test_dataset,
+            sampler = RandomSampler(train_dataset), 
+            batch_size = batch_size 
+        ) 
 
 
 def main():
