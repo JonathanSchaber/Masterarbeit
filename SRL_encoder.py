39c73188cc8b44ad68441df65f19c74675e3bd45
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-08-04 22:22:34 +0200

    continued

--- SRL_encoder.py
+++ SRL_encoder.py
@@ -1,10 +1,19 @@
 import torch
 
 class SRL_Encoder(nn.Module):
-    def __init__(self, num_labels, embedding_dim):
+    def __init__(self, num_labels=47, embedding_dim):
         super(SRL_Encoder, self)__init__()
         self.embeddings = nn.Embedding(num_labels, embedding_dim)
-        self.encoder = nn.GRU()
+        self.encoder = nn.GRU(
+                            input_size=embedding_dim,
+                            hidden_size,
+                            num_layers,
+                            bias=True,
+                            batch_first,
+                            dropout,
+                            bidirectional
+                            )
 
     def forward(self):
-        pass
+        embeddings = self.embeddings
+        output, h_n = self.encoder(embeddings)
