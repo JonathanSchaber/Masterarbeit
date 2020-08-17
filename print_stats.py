fb708faec3876b281d320f8e3f55313a05da1a45
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-08-17 18:56:18 +0200

    updated function

--- print_stats.py
+++ print_stats.py
@@ -2,10 +2,30 @@
 import matplotlib.pyplot as plt
 
 
-def make_pies(labels, values, font_size, title):
+def make_pies(labels, values, colors, font_size=10, title=None):
     fig, ax = plt.subplots()
-    a, b, c, = ax.pie(values, labels=labels, autopct='%1.1f%%', startangle=90)
+    a, b, _, = ax.pie(values, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
     [ _.set_fontsize(font_size) for _ in b ]
-    plt.title(title, bbox={'facecolor':'0.8', 'pad':5})
+    if title:
+        plt.title(title, bbox={'facecolor':'0.8', 'pad':5})
     plt.show()
     
+def make_pies(labels, values, colors, font_size=10, title=None):
+      fig, ax = plt.subplots()
+      a, b, _, = ax.pie(values, colors=colors, pctdistance=0.6, autopct='%1.1f%%', startangle=90)
+      [ _.set_fontsize(font_size) for _ in b ]
+      ax.legend(a, labels, loc="center right", bbox_to_anchor=(1.7, 0.5), fontsize="x-large")
+      if title:
+          plt.title(title, bbox={'facecolor':'0.9', 'pad':5})
+      plt.show()
+
+# def make_nested_pies(labels_out, colors_out, values_out, labels_in, colors_in, values_in):
+#     fig, ax = plt.subplots()
+#     ax.axis('equal')
+#     width = 0.3
+#     pie, _, _ = ax.pie(values_out, radius=1, labels=labels_out, colors=colors_out, autopct='%1.1f%%', startangle=90)
+#     plt.setp( pie, width=width, edgecolor='white')
+#     pie2, _, _ = ax.pie(values_in, radius=1-width, labels=labels_in, colors=colors_in, autopct='%1.1f%%', startangle=90)
+#     plt.setp( pie2, width=width, edgecolor='white')
+#     plt.show()
+# 
