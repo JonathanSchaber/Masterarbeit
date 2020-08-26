21cfd1e262fe2d41be07089219d2ed86303fdb5d
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-08-26 11:44:31 +0200

    added doc string

--- max_acc.sh
+++ max_acc.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# place somewhere in system path to execute from cmd-line (e.g. ~/.local/bin)
+# Synopsis: $ max_acc.sh <file>
+
 FILE=${1}
 
 MAX_DEV=$(jq '.' "$FILE" | grep 'Dev Acc' | sort | tail -n 1 | grep -Po '0\.\d+')
