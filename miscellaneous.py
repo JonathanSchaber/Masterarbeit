fb51f86ff430bf5e03d08916969f3cf6dac4d997
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-08-29 15:39:33 +0200

    added function for computing stats for QA data sets

--- miscellaneous.py
+++ miscellaneous.py
@@ -29,3 +29,27 @@ def make_pies(labels, values, colors, font_size=10, title=None):
 #     plt.setp( pie2, width=width, edgecolor='white')
 #     plt.show()
 # 
+
+
+def vals(data):
+    """function to compute length of strings for QA data sets
+    Args:
+        param1: list[list]
+    """
+     answer_normal = 0
+     question_normal = 0
+     context_normal = 0
+     answer_token = 0
+     question_token = 0
+     context_token = 0
+     for x in data:
+         answer_token += len(tokenizer.tokenize(x[1]))
+         question_token += len(tokenizer.tokenize(x[3]))
+         context_token += len(tokenizer.tokenize(x[2]))
+         answer_normal += len([y for y in tokenizer.tokenize(x[1]) if not y.startswith("##")])
+         question_normal += len([y for y in tokenizer.tokenize(x[3]) if not y.startswith("##")])
+         context_normal += len([y for y in tokenizer.tokenize(x[2]) if not y.startswith("##")])
+     print("av. length answer: {:.1f} ({:.1f})".format(answer_normal/len(data), answer_token/len(data)))
+     print("av. length question: {:.1f} ({:.1f})".format(question_normal/len(data), question_token/len(data)))
+
+     print("av. length context: {:.1f} ({:.1f})".format(context_normal/len(data), context_token/len(data)))
