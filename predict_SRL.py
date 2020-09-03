8333102bd636101da91e3cbe0b99b3f103cf2fc9
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-09-03 08:50:22 +0200

    added empty-SRL symbol for sentences with no predicates

--- predict_SRL.py
+++ predict_SRL.py
@@ -11,6 +11,9 @@ from transformers import BertTokenizer
 import parzu_class as parzu
 
 
+# /home/joni/Documents/Uni/Master/Computerlinguistik/20HS_Masterarbeit/SemRolLab/DAMESRL/server_configs/srl_char_att_ger_infer.ini
+
+
 class SRL_predictor:
     def __init__(self, argument_model_config, bert_path):
         self.dsrl = DSRL(argument_model_config)
@@ -34,7 +37,12 @@ class SRL_predictor:
         dsrl_text = Text()
 
         for sentence in sentences:
-            dsrl_sentence = Sentence([Word(tuple[0]) if tuple[1] != "PRED" else Predicate(Word(tuple[0])) for tuple in sentence])
+            dsrl_sentence = Sentence(
+                                [Word(tuple[0])
+                                    if tuple[1] != "PRED"
+                                    else Predicate(Word(tuple[0])) 
+                                    for tuple in sentence]
+                                )
             dsrl_text.append(dsrl_sentence)
         
         for sentence in dsrl_text:
@@ -92,8 +100,7 @@ class SRL_predictor:
     def parse_text(self, text):
         """parse sentence and return list of tuples with token and POS-tags
         Args:
-            param1: parser Object
-            param2: sentence to parse
+            param1: text to parse
         Returns:
             list of lists of tuples of strings
         """
@@ -126,8 +133,8 @@ class SRL_predictor:
             #tagged_tuple_list.append(srl_sentence)
 
             # since ParZu tokenizes slightly different then Bert,
-            # we tokenize Bert-like and only use the obtained information
-            # about what is a predicate
+            # we tokenize Bert-like, merge to token-level, and only use the obtained information
+            # about what is a predicate from the ParZu-Parser output
             bert_tokenizer_list = self.merge_subtokens(
                                         self.tokenizer.tokenize(
                                             " ".join([x[0] for x in srl_sentence])
@@ -160,17 +167,19 @@ class SRL_predictor:
         parsed_text = self.parse_text(text)
         dsrl_obj = self.create_dsrl_repr(parsed_text)
 
+        # if there are no predicates in all sentences, return empty-SRLs for
+        # all sentences
         try:
             sem_roles = self.dsrl.predict(dsrl_obj)
         except IndexError:
             for i in range(len(dsrl_obj)):
-                srl_list.append([["O"]*len(dsrl_obj[i])])
+                srl_list.append([["0"]*len(dsrl_obj[i])])
             return srl_list
 
         for sent in sem_roles:
             sent_list = []
             if sent.get_predicates() == []:
-                sent_list.append(["O"]*len(sent))
+                sent_list.append(["0"]*len(sent))
             else:
                 for predicate in sent.get_predicates():
                     sent_list.append(predicate.arguments) 
