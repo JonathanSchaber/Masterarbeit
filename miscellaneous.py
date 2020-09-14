34f7ed9a0b3ef57ad011d1810ae2f5a23fd39c0c
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-09-14 09:51:47 +0200

    added new functions for plotting loss, accuracy

--- miscellaneous.py
+++ miscellaneous.py
@@ -1,5 +1,25 @@
-# coding: utf-8
+import argparse
+import json
 import matplotlib.pyplot as plt
+
+
+def parse_cmd_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            "-s", 
+            "--stats_file", 
+            type=str, 
+            help="Path to stats file"
+            )
+    parser.add_argument(
+            "-f",
+            "--feature",
+            type = str,
+            choices = ["loss", "accuracy"],
+            help = "Which feature to plot"
+            )
+    return parser.parse_args()
 
 
 def make_pies(labels, values, colors, font_size=10, title=None):
@@ -36,20 +56,82 @@ def vals(data):
     Args:
         param1: list[list]
     """
-     answer_normal = 0
-     question_normal = 0
-     context_normal = 0
-     answer_token = 0
-     question_token = 0
-     context_token = 0
-     for x in data:
-         answer_token += len(tokenizer.tokenize(x[1]))
-         question_token += len(tokenizer.tokenize(x[3]))
-         context_token += len(tokenizer.tokenize(x[2]))
-         answer_normal += len([y for y in tokenizer.tokenize(x[1]) if not y.startswith("##")])
-         question_normal += len([y for y in tokenizer.tokenize(x[3]) if not y.startswith("##")])
-         context_normal += len([y for y in tokenizer.tokenize(x[2]) if not y.startswith("##")])
-     print("av. length answer: {:.1f} ({:.1f})".format(answer_normal/len(data), answer_token/len(data)))
-     print("av. length question: {:.1f} ({:.1f})".format(question_normal/len(data), question_token/len(data)))
+    answer_normal = 0
+    question_normal = 0
+    context_normal = 0
+    answer_token = 0
+    question_token = 0
+    context_token = 0
+    for x in data:
+        answer_token += len(tokenizer.tokenize(x[1]))
+        question_token += len(tokenizer.tokenize(x[3]))
+        context_token += len(tokenizer.tokenize(x[2]))
+        answer_normal += len([y for y in tokenizer.tokenize(x[1]) if not y.startswith("##")])
+        question_normal += len([y for y in tokenizer.tokenize(x[3]) if not y.startswith("##")])
+        context_normal += len([y for y in tokenizer.tokenize(x[2]) if not y.startswith("##")])
+    print("av. length answer: {:.1f} ({:.1f})".format(answer_normal/len(data), answer_token/len(data)))
+    print("av. length question: {:.1f} ({:.1f})".format(question_normal/len(data), question_token/len(data)))
 
-     print("av. length context: {:.1f} ({:.1f})".format(context_normal/len(data), context_token/len(data)))
+    print("av. length context: {:.1f} ({:.1f})".format(context_normal/len(data), context_token/len(data)))
+
+
+def plot_losses(log_file):
+    """
+    Plot losses of train, dev and test set over all epochs
+    Args:
+        param1: JSON-file
+    """
+    with open(log_file, "r") as f:
+        stats_file = f.read()
+    stats_file = json.loads(stats_file)
+    train_loss = []
+    dev_loss = []
+    test_loss = []
+    for epoch in stats_file[2:]:
+        train_loss.append(epoch["Train Loss"])
+        dev_loss.append(epoch["Dev Loss"])
+        test_loss.append(epoch["Test Loss"])
+    plt.plot(train_loss, "b", label="Training Loss")
+    plt.plot(dev_loss, "g", label="Development Loss")
+    plt.plot(test_loss, "y", label="Test Loss")
+    plt.style.use("Solarize_Light2")
+    plt.xlabel("Epochs")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    plt.show()
+
+
+def plot_accuracy(log_file):
+    """
+    Plot losses of train, dev and test set over all epochs
+    Args:
+        param1: JSON-file
+    """
+    with open(log_file, "r") as f:
+        stats_file = f.read()
+    stats_file = json.loads(stats_file)
+    train_acc = []
+    dev_acc = []
+    test_acc = []
+    for epoch in stats_file[2:]:
+        train_acc.append(epoch["Train Accur."])
+        dev_acc.append(epoch["Dev Accur."])
+        test_acc.append(epoch["Test Accur."])
+    plt.plot(train_acc, "b", label="Training Accuracy")
+    plt.plot(dev_acc, "g", label="Development Accuracy")
+    plt.plot(test_acc, "y", label="Test Accuracy")
+    plt.style.use("Solarize_Light2")
+    plt.xlabel("Epochs")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    plt.show()
+
+
+def main():
+    args = parse_cmd_args()
+    if args.feature == "loss":
+        plot_losses(args.stats_file)
+    elif args.feature == "accuracy":
+        plot_accuracy(args.stats_file)
+
+    
+if __name__ == "__main__":
+    main()
