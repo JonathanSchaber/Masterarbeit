4dcf450151b2a1be2f85dd4e5e431cd4b2d95015
Jonathan Schaber <jonathan.schaber@gmx.ch>
2020-10-23 13:32:18 +0200

    added typing

--- ensemble_predict.py
+++ ensemble_predict.py
@@ -1,7 +1,9 @@
 import argparse
 import json
 import subprocess
+
 from pathlib import Path
+from typing import Dict, List, Tuple
 
 
 download_file = "scp jschaber@rattle.ifi.uzh.ch:~/MA/results/{} /home/joni/Documents/Uni/Master/Computerlinguistik/20HS_Masterarbeit/Masterarbeit/results/"
@@ -23,8 +25,14 @@ def parse_cmd_args():
     return parser.parse_args()
 
 
-def read_in_files(files):
-    """
+def read_in_files(files: List[str])
+            -> List[Tuple[List[Dict[str, float]]], Tuple[List[Dict[str, float]]]]:
+    """Reads in the files, return json-loaded objects
+    Args:
+        files: list of n stats-files
+    Returns:
+        json_files: json-loaded stats and corresponding results files
+    
     """
     json_files = []
     
@@ -58,7 +66,13 @@ def read_in_files(files):
 
 
 def get_best_epoch(stats_file):
-    """
+    """Find the best epoch according to Dev Accuracy
+    Args:
+        stats_file: json-file of all epoch results
+    Returns:
+        best_epch: number of the best epoch (starting from 1)
+        best_dev: corresponding Dev Accuracy
+        best_test: corresponding Test Accuracy
     """
     best_epoch = 1
     best_dev = 0.0
@@ -74,6 +88,25 @@ def get_best_epoch(stats_file):
     return best_epoch, best_dev, best_test
 
 
+def build_dicts(results: Tuple[List[List[str]]])
+            -> Tuple[Dict[str, List[str]], Dict[str, str]]:
+    """build dictionaries; one with all preds, one gold
+    Args:
+        results: dictionary of pred, golds of best epoch
+    """
+    target_dict, target_gold_dict = {}, {}
+    for element in results:
+        if element[0] in target_dict:
+            target_dict[element[0]].append(element[1])
+            if not target_gold_dict[element[0]] == element[2]:
+                import ipdb; ipdb.set_trace()
+        else:
+            target_dict[element[0]] = [element[1]]
+            target_gold_dict[element[0]] = element[2]
+
+    return target_dict, target_gold_dict
+
+    
 def main():
     args = parse_cmd_args()
     files = args.result_files
@@ -84,8 +117,6 @@ def main():
         import ipdb; ipdb.set_trace()
 
     ensemble_results = []
-    dev_dict, test_dict = {}, {}
-    dev_gold_dict, test_gold_dict = {}, {}
     
     for i, file_pair in enumerate(jsons):
             stats_file, results_file = file_pair
@@ -105,26 +136,28 @@ def main():
         
     for results in ensemble_results:
         dev_res, test_res = results
-        for element in dev_res:
-            if element[0] in dev_dict:
-                dev_dict[element[0]].append(element[1])
-                if not dev_gold_dict[element[0]] == element[2]:
-                    import ipdb; ipdb.set_trace()
-            else:
-                dev_dict[element[0]] = [element[1]]
-                dev_gold_dict[element[0]] = element[2]
-        for element in test_res:
-            if element[0] in test_dict:
-                test_dict[element[0]].append(element[1])
-                if not test_gold_dict[element[0]] == element[2]:
-                    import ipdb; ipdb.set_trace()
-            else:
-                test_dict[element[0]] = [element[1]]
-                test_gold_dict[element[0]] = element[2]
+
+        dev_dict, dev_gold_dict = build_dicts(dev_res)
+        test_dict, test_gold_dict = build_dicts(test_res)
+        #for element in dev_res:
+        #    if element[0] in dev_dict:
+        #        dev_dict[element[0]].append(element[1])
+        #        if not dev_gold_dict[element[0]] == element[2]:
+        #            import ipdb; ipdb.set_trace()
+        #    else:
+        #        dev_dict[element[0]] = [element[1]]
+        #        dev_gold_dict[element[0]] = element[2]
+        #for element in test_res:
+        #    if element[0] in test_dict:
+        #        test_dict[element[0]].append(element[1])
+        #        if not test_gold_dict[element[0]] == element[2]:
+        #            import ipdb; ipdb.set_trace()
+        #    else:
+        #        test_dict[element[0]] = [element[1]]
+        #        test_gold_dict[element[0]] = element[2]
     import ipdb; ipdb.set_trace()
-        
-
-
+ 
+     
 if __name__ == "__main__":
     main()
     
