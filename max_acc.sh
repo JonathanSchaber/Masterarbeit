5d2278bf3c2529ac19c379d1f88d6bc2ac53d7de
Jonathan Schaber <jonathan.schaber@gmx.ch>
2021-03-05 08:02:10 +0100

    small change to adhere to standard syntax

--- max_acc.sh
+++ max_acc.sh
@@ -7,7 +7,7 @@ FILE=${1}
 
 MAX_DEV=$(jq '.' "$FILE" | grep 'Dev Acc' | sort | tail -n 1 | grep -Po '0\.\d+')
 
-if [ -z "$MAX_DEV" ]
+if [[ -z "$MAX_DEV" ]]
 then
 	echo "Old json file, grep manually..."
 else
